692b8e44d09da47553e8266194db124691a5aeba
oi111 <shifabinoi@gmail.com>
2017-03-23 20:35:43 +0800

    add Uti.Uti

---
+++ src/Uti/Uti.java
@@ -0,0 +1,22 @@
+package Uti;
+
+import java.util.List;
+
+public class Uti {
+
+	static double calSigma(List<Double> ld) {
+		double averager = calAverage(ld);
+		double tot = 0;
+		for (int i = 0; i < ld.size(); i++)
+			tot += (ld.get(i) - averager) * (ld.get(i) - averager);
+		tot /= (ld.size() + 1e-8);
+		return Math.sqrt(tot);
+	}
+
+	static double calAverage(List<Double> ld) {
+		double tot = 0;
+		for (int i = 0; i < ld.size(); i++)
+			tot += ld.get(i);
+		return tot / (ld.size() + 1e-8);
+	}
+}
