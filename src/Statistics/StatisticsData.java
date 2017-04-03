df1e7f7bc2f32f1c5d32380589f9694a73013714
oi111 <shifabinoi@gmail.com>
2017-04-03 09:27:05 +0800

    add new file StatisticsData

--- src/Statistics/StatisticsData.java
+++ src/Statistics/StatisticsData.java
@@ -8,8 +8,8 @@ import Uti.InputFile;
 import Uti.OutputFile;
 
 public class StatisticsData {
-	int a[] = new int[300];
-	String b[] = new String[300];
+	int a[] = new int[400];
+	String b[] = new String[400];
 	HashMap<String, Integer> hm = new HashMap<String, Integer>();
 
 	public static void main(String[] args) {
@@ -30,23 +30,32 @@ public class StatisticsData {
 		outputValue(outputfile + "Value.txt");
 	}
 
+	void buildTime(long time) {
+		hm.clear();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		for (int i = 0; i < 350; i++) {
+			Date date = new Date(time + 24 * 60 * 60 * 1000 * i);
+			hm.put(sdf.format(date), i);
+			b[i] = sdf.format(date);
+		}
+
+	}
+
 	void read(String file) {
-		HashMap<String, Integer> hm = new HashMap<String, Integer>();
 		InputFile input = new InputFile();
 		input.setFileName(file);
 		input.openFile();
 		String line = null;
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
 		int index = 0;
+		line = input.read();
+		String tmp[] = line.split(" ");
+		buildTime(Long.valueOf(tmp[0]));
 		while ((line = input.read()) != null) {
 			String pt[] = line.split(" ");
 			Date date = new Date(Long.valueOf(pt[0]));
 			String str = sdf.format(date);
-			if (!hm.containsKey(str)) {
-				hm.put(str, ++index);
-				b[index] = str;
-				System.out.println(index);
-			}
 			a[hm.get(str)]++;
 		}
 		input.closeFile();
