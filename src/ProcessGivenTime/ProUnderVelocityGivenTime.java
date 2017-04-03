86fb83641e117a39ed9e09fdb062f6b947230b2b
oi111 <shifabinoi@gmail.com>
2017-04-03 09:29:51 +0800

    1

--- src/ProcessGivenTime/ProUnderVelocityGivenTime.java
+++ src/ProcessGivenTime/ProUnderVelocityGivenTime.java
@@ -3,6 +3,7 @@ package ProcessGivenTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import Data.ITK;
 import Data.StrOrder;
 import Uti.InputFile;
 import Uti.OutputFile;
@@ -15,6 +16,7 @@ public class ProUnderVelocityGivenTime {
 	int LL = 10000;
 	int CC = 1;
 	int BIN = 1;
+	String OTHER = "";
 
 	int index = -1;
 	int a[][];
@@ -23,13 +25,15 @@ public class ProUnderVelocityGivenTime {
 	int mid[];
 	StrOrder pre = null, current = null;
 	List<List<List<Double>>> ld3 = new ArrayList<List<List<Double>>>();
+	List<List<ITK>> tltk = new ArrayList<List<ITK>>();
+
 	int ak1 = 0, ak2 = 0;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ProUnderVelocityGivenTime pu = new ProUnderVelocityGivenTime();
 		pu.process(args[0], Integer.valueOf(args[1]), Integer.valueOf(args[2]), Integer.valueOf(args[3]),
-				Integer.valueOf(args[4]), Integer.valueOf(args[5]), Integer.valueOf(args[6]));
+				Integer.valueOf(args[4]), Integer.valueOf(args[5]), Integer.valueOf(args[6]), args[7]);
 	}
 
 	void init() {
@@ -49,19 +53,24 @@ public class ProUnderVelocityGivenTime {
 		index = (TT - 1);
 		for (int i = 0; i < KK; i++) {
 			List<List<Double>> tmp = new ArrayList<List<Double>>();
+			List<ITK> ti = new ArrayList<ITK>();
 			tmp.clear();
-			for (int j = 0; j < NN / CC; j++)
+			for (int j = 0; j < NN / CC; j++) {
 				tmp.add(new ArrayList<Double>());
+				ti.add(new ITK(0, 0));
+			}
 			ld3.add(tmp);
+			tltk.add(ti);
 		}
 	}
 
-	void process(String inputfile, int limit, int bin, int cc, int kk, int ll, int tt) {
+	void process(String inputfile, int limit, int bin, int cc, int kk, int ll, int tt, String other) {
 		this.KK = kk;
 		this.TT = tt;
 		this.LL = ll;
 		this.BIN = bin;
 		this.CC = cc;
+		this.OTHER = other;
 		init();
 		InputFile input = new InputFile();
 		input.setFileName(inputfile);
@@ -79,8 +88,12 @@ public class ProUnderVelocityGivenTime {
 		}
 		System.out.println(ak1 + " " + ak2);
 		input.closeFile();
-		outputData("ProUnderVelocityGivenTime_T" + TT + "B" + this.BIN + "C" + this.CC + "K" + this.KK + "L" + LL);
-		outputPro("ProUnderVelocityGivenTime_T" + TT + "B" + this.BIN + "C" + this.CC + "K" + this.KK + "L" + LL);
+		outputTime("ProUnderVelocityGivenTime_T" + TT + "B" + this.BIN + "C" + this.CC + "K" + this.KK + "L" + LL + "OT"
+				+ OTHER);
+		outputData("ProUnderVelocityGivenTime_T" + TT + "B" + this.BIN + "C" + this.CC + "K" + this.KK + "L" + LL + "OT"
+				+ OTHER);
+		outputPro("ProUnderVelocityGivenTime_T" + TT + "B" + this.BIN + "C" + this.CC + "K" + this.KK + "L" + LL + "OT"
+				+ OTHER);
 	}
 
 	void addOne(String line) {
@@ -105,13 +118,29 @@ public class ProUnderVelocityGivenTime {
 		if (v + KK / 2 >= 0 && v + KK / 2 < KK)
 			ak1++;
 		if (v + KK / 2 >= 0 && v + KK / 2 < KK && p1.time / 1000 - time[index] / 1000 <= TT * (1 + 0.05)) {
-			for (int i = 0; i < pt.length; i += CC)
-				if (ld3.get(v + KK / 2).get(i / CC).size() < LL && sum[i] != 0)
+			for (int i = 0; i < pt.length; i += CC) {
+				tltk.get(v + KK / 2).get(i / CC).t1++;
+				if (ld3.get(v + KK / 2).get(i / CC).size() < LL && sum[i] != 0) {
 					ld3.get(v + KK / 2).get(i / CC).add((double) (sum[i] / 1000.0));
+					tltk.get(v + KK / 2).get(i / CC).t2++;
+				}
+			}
 			ak2++;
 		}
 		mid[index] = p1.mid;
 		time[index] = p1.time;
+	}
+
+	void outputTime(String outputfile) {
+		OutputFile output = new OutputFile();
+		output.setFileName(outputfile + "Time.txt");
+		output.openFile();
+		for (int i = 0; i < tltk.size(); i++) {
+			for (int j = 0; j < tltk.get(i).size(); j++)
+				output.write(tltk.get(i).get(j).t2 / (tltk.get(i).get(j).t1 + 1e-8) + " ");
+			output.write("\n");
+		}
+		output.closeFile();
 	}
 
 	void outputData(String outputfile) {
